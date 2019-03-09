39353115b2a8596545ec831b283ebc8b00ba7186
Pierpaolo <plucarelli.94@gmail.com>
2019-03-09 16:23:23 +0100

    gradient magnitude and orientation

---
+++ linearFilters/gradmag.m
@@ -0,0 +1,16 @@
+function [mag, or] = gradmag(img, sigma)
+    img = rgb2gray(img);
+    dgx = gaussianDer(gaussian(sigma), sigma);
+    dgy = gaussianDer(gaussian(sigma), sigma);
+    xkernel = zeros(length(dgx),length(dgy));
+    xkernel(floor((length(dgx)/2)+1), :) = dgx;
+    ykernel = xkernel.';
+    xGrad = conv2(img, xkernel, 'same');
+    yGrad = conv2(img, ykernel, 'same');
+    
+    % calculate magnitude of gradient
+    mag = sqrt(xGrad.^2 + yGrad.^2);
+    
+    % calculate orienation of gradient
+    or = atan2(xGrad, yGrad);
+end
--- linearFilters/main.m
+++ linearFilters/main.m
@@ -1,3 +1,11 @@
 imIn = imread('zebra.png');
 imOut = gaussianConv(imIn, 3, 3);
 imshow(imOut);
+
+%%
+[mag, orientation]= gradmag(imIn, 2);
+i = rescale(mag, 0, 255);
+imshow(uint8(i));
+imshow(orientation , [-pi,pi]);
+colormap(hsv);
+colorbar;
